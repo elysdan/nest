93ac887c39022b84fc9873c006c08cfa03a61823
elysdan <elysdan@vivaldi.net>
2025-01-27 18:27:30 -0400

    agregar

---
+++ main.js
@@ -0,0 +1,31 @@
+function restar (a, b){
+    return a - b
+}
+
+// git init, Iniciar el repositorio.
+
+// ADD, git Add ., agregar cambios en stage
+
+// PULL, git pull, traerte los cambios
+
+// PUSH, git push, subir cambios
+
+// COMMIT, git commit <nombre> Guardar cambios
+
+
+
+
+// FETCH, Actualizar todas las ramas.
+
+// BRANCH, para ver ramas
+
+// CHECKOUT, cambiar de rama
+
+// CLONE, clonar un repositorio
+
+
+// MERGE, unir dos versiones de codigo 
+
+
+
+// cambio -> Staging / Stage -> Commit
